be244cf23e42e2b120f9f40f983e489f70ac604e
moriyamasaaaki <moriya-7071@outlook.com>
2020-01-20 15:43:08 +0900

    feat(angular fire): 追加 #14

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBFHcGu59qTzQ55Ieo-Yg35_4uw0aNtEWE',
+    authDomain: 'todo-app-58cf0.firebaseapp.com',
+    databaseURL: 'https://todo-app-58cf0.firebaseio.com',
+    projectId: 'todo-app-58cf0',
+    storageBucket: 'todo-app-58cf0.appspot.com',
+    messagingSenderId: '115468213124',
+    appId: '1:115468213124:web:74cc3c90c665f5e3f45bd1',
+    measurementId: 'G-R676FBL0CE'
+  }
 };
 
 /*
