cd3193a9b584ca7baa8139d26ea745d0f0264a9d
moriyamasaaaki <moriya-7071@outlook.com>
2020-05-06 23:09:58 +0900

    feat(todo): module import #33

--- src/app/interfaces/todo.ts
+++ src/app/interfaces/todo.ts
@@ -1,8 +1,10 @@
+import { firestore } from 'firebase';
+
 export interface Todo {
   userId: string;
   id: string;
   title: string;
-  createdAt: Date;
-  category: string;
+  createdAt: firestore.Timestamp;
+  category: '仕事' | 'プライベート' | 'その他';
   content: string;
 }
